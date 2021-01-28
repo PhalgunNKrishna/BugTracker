0d4ca26841edf51f7aa851a48c606b6abdd810a1
Phalgun Krishna <pnkrishna@ucdavis.edu>
2021-01-28 11:53:54 -0800

    added more to user html

--- target/classes/static/app/users.controller.js
+++ target/classes/static/app/users.controller.js
@@ -11,7 +11,34 @@ app.controller("UsersController", function($scope, $http) {
         $http.get("http://localhost:7070/user/" + tid)
         .success(function(p) {
             $scope.p = p;
-            console.log("page");
+            //var mess = JSON.parse(p);
+            const mess = JSON.parse(JSON.stringify(p));
+            var admin_message = mess.admin_message;
+
+            console.log("admin message = " + admin_message);
+
+            if (admin_message.length > 40) {
+                split_admin = admin_message.split(" ");
+                var count = 0;
+                var res = "";
+                for (var i = 0; i < split_admin.length; i++) {
+                    while (count < 40) {
+                        res.concat(split_admin[i]);
+                        res.concat(" ");
+                        console.log("res = " + res);
+                        count = count + split_admin[i].length + 1;
+                        console.log("res = " + res);
+                     } res.concat("<br>");
+                     count = 0;
+                }
+           } else {
+                var res = admin_message;
+           }
+
+           console.log(res);
+
+           $scope.ad_mess = res;
+
         });
     };
 
